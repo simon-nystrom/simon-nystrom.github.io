8a9e732d901fa418e8ea1e1f79a47327d8127b6a
Simon Nyström <simon.nystrom90@gmail.com>
2020-11-04 22:38:35 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b8d63ba2531791c1b11d";
+/******/ 	var hotCurrentHash = "e2aa38b4e4cb8983767b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
