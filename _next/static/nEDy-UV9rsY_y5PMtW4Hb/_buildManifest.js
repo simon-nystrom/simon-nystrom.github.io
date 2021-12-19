4443815b01f25b47723236539496066de110de68
Simon Nyström <simon.nystrom90@gmail.com>
2021-12-19 20:05:03 +0100

    Updates

---
+++ _next/static/nEDy-UV9rsY_y5PMtW4Hb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1f0ff059d79eda2a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/posts/tldr":["static/chunks/pages/posts/tldr-bf584c7c98e10390.js"],sortedPages:["/","/_app","/_error","/posts/tldr"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
