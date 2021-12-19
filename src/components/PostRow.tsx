2857622520319163b8f67748fe6c18eee05c7edd
Simon Nyström <simon.nystrom90@gmail.com>
2021-12-19 20:04:45 +0100

    fixes

--- src/components/PostRow.tsx
+++ src/components/PostRow.tsx
@@ -5,11 +5,11 @@ import { PostMeta } from "./PostList";
 
 const PostRow = ({ post }: { post: PostMeta }) => (
   <Link passHref href={`/posts/${post.slug}`}>
-    <div className="flex justify-between border px-3 py-1 rounded-md border-opacity-20 border-orange-400 transition-all hover:border-opacity-100 duration-150">
-      <Headers.H2>{post.title}</Headers.H2>
+    <div className="flex flex-col md:flex-row justify-between border px-3 py-2 rounded-md border-opacity-20 border-orange-400 transition-all hover:border-opacity-100 duration-150">
+      <h2 className="text-2xl">{post.title}</h2>
       <div className="flex flex-col items-end justify-center gap-2">
-        <p className="text-sm">{post.date}</p>
-        <div className="flex-col hidden md:flex items-end md:flex-row gap-2">
+        <p className="text-sm mt-2 text-gray-400">{post.date}</p>
+        <div className="flex flex-row items-end gap-2">
           <Tag idx={0} text={post.series}></Tag>
           {post.tags.map((tag, i) => (
             <Tag key={tag} idx={i + 1} text={tag}></Tag>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Nav from "../components/Nav";
+import Head from "next/head";
 
 function NewcurrentApp({ Component, pageProps }: AppProps) {
   return (
     <div className="p-4">
+      <Head>
+        <title>Newcurrent | A curious software engineer</title>
+        <meta
+          name="description"
+          content="Newcurrent is the blog of a software engineer with a passion for learning and developing as a professional. I write about all kinds of topics related to software engineering / programming."
+        />
+      </Head>
       <Nav></Nav>
       <Component {...pageProps} />
     </div>
